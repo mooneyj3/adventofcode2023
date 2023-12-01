5e6f218aca67d3b76028498dabb2e9afb346ca9f
Jonny Mooneyham <mooneyj3@wwu.edu>
2023-12-01 12:25:01 -0800

    completed day01 part 2 solution

--- day01/day01.py
+++ day01/day01.py
@@ -3,6 +3,30 @@ import re
 def extract_first_last(line):
     return int(re.search(r"\d", line).group() + re.search(r"(\d)(?!.*\d)", line).group())
 
+string_to_num = {
+    # "oneight" : "18",
+    # "threeight" : "38",
+    # "fiveight" : "58",
+    # "sevenine" : "79",
+    # "eightwo" : "82",
+    # "eighthree" : "83",
+    # "nineight" : "98",
+    "one"  : "o1ne",
+    "two"  : "t2we",
+    "three": "th3ree",
+    "four" : "fo4ur",
+    "five" : "fi5ve",
+    "six"  : "s6ix",
+    "seven": "se7ven",
+    "eight": "ei8ght",
+    "nine" : "ni9ne"
+}
+
+def string_alpha_to_num(line):
+    for key, value in string_to_num.items():
+        line = line.replace(key, value)
+    return line
+
 # PART 1
 def solve_part_1(f):
     running_total = 0
@@ -11,10 +35,21 @@ def solve_part_1(f):
     return running_total
 
 # PART 2
+def solve_part_2(f):
+    # The right calibration values for string "eighthree" is 83 and for "sevenine" is 79.
+    running_total = 0
+    for l in f:
+        new_l = string_alpha_to_num(l)
+        num = extract_first_last(new_l)
+        running_total += num
+    return running_total
 
 
 if __name__ == "__main__":
-    input_file = "day01.input"
+    input_file = "day01/day01.input"
     with open(input_file, 'r') as f:
         result = solve_part_1(f)
         print("DAY 1 (part 1): %d" % (result))
+    with open(input_file, 'r') as f:
+        result = solve_part_2(f)
+        print("DAY 1 (part 2): %d" % (result))
