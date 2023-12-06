62559c46af76c7e0d0c87d1ffb38d5a69d81b30a
Jonny Mooneyham <mooneyj3@wwu.edu>
2023-12-05 22:14:13 -0800

    solved day05 part 1

--- day05/main.py
+++ day05/main.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 
 from soilent_green import get_lowest_location_number, parse_input_data
+INPUT_FILE = "day05.input"
+
+def data_to_string(input_data: str, is_file: bool = True) -> str:
+    lines = None
+    if is_file:
+        with open(input_data, 'r') as f:
+            lines = f.read()
+    else:
+        lines = input_data
+    return lines
 
 def main():
-    return None
+    input_data = data_to_string(INPUT_FILE)
+    almanac = parse_input_data(input_data)
+    lowest_location = get_lowest_location_number(almanac)
+    print("DAY 5 (part 1): %d" % (lowest_location))
+
 
 if __name__ == "__main__":
     main()
--- day05/soilent_green/soilent_green_is_elves.py
+++ day05/soilent_green/soilent_green_is_elves.py
@@ -27,9 +27,74 @@
 # so they'd like to know the closest location that needs a seed. Using these maps, 
 # find the lowest location number that corresponds to any of the initial seeds.
 
-def parse_input_data():
-    return None
+def slice_int_array(int_str: str) -> list[str]:
+    return [int(l) for l in int_str.strip().split()]
+
+def parse_input_data(input_data: list[str]) -> dict:
+    current_map = None
+    almanac = {}
+    for line in input_data.split('\n'):
+        if line.strip() == '':
+            current_map = None
+            continue
+        elif 'map' in line and current_map is None: 
+            # note this doesn't check that the dictionary key disn't already set/exists
+            current_map = line[0:line.index(" map")].strip().replace("-to-", "|")
+            almanac[current_map] = []
+            continue
+        elif 'seeds' in line and current_map is None:
+            almanac["seeds"] = slice_int_array(line[line.index(':')+1:])
+            continue
+        else:
+           almanac[current_map].append(slice_int_array(line)) 
+    return almanac
+
+def get_next_dest_key(almanac: dict, prefix: str) -> str:
+    # TODO: create short function to get the next mapping from the dictionary keys
+    for key in almanac.keys():
+        if key.startswith(prefix):
+            return key.split('|')[1]
+    return "You done goofed!"
+
+# Each line within a map contains three numbers: 
+#        the destination range start, 
+#        the source range start, 
+#        and the range length.
+def get_final_destination(start_num: int, almanac: dict, src_key: str = 'seed', dest_key: str = 'soil') -> int:
+
+    dest_num = start_num
+    
+    # print(src_key + '|' + dest_key)
+    # here we are recursing through the lists
+    for m in almanac[src_key + '|' + dest_key]:
+        #check if seed is in cource range
+        if start_num >= m[1] and start_num < m[1] + m[2]:
+            offset = start_num - m[1]
+            dest_num = start_num - m[1] + m[0]
+            break
+    
+    # if dest_num is not None:
+    #     dest_num = start_num
+    # # print(dest_num, end=" ")
+
+    if dest_key == 'location':
+        return dest_num
+    
+    next_dest_key = get_next_dest_key(almanac, dest_key)
+    
+    return get_final_destination(dest_num, almanac, dest_key, next_dest_key)
 
 # PART 1
-def get_lowest_location_number():
-    return None
+def get_lowest_location_number(almanac: dict) -> int:
+    
+    seeds = almanac.pop('seeds')
+    final_locs = []
+    
+    # for seed in almanac["seeds"]:
+    for seed in seeds:
+        # print("SEED [" + str(seed) + "] : ", end='')
+        final_locs.append(get_final_destination(seed, almanac))
+
+    # print(final_locs)
+
+    return min(final_locs)
